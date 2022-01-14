9921f772cd0c6c04439e398c45c40e80a62816ff
应涛 <yt1577396227@163.com>
2022-01-14 20:43:44 +0800

    feat: 新增抢购接口限流

--- src/main/java/com/yt/seckill/controller/TOrderRecordController.java
+++ src/main/java/com/yt/seckill/controller/TOrderRecordController.java
@@ -1,6 +1,7 @@
 package com.yt.seckill.controller;
 
 
+import com.google.common.util.concurrent.RateLimiter;
 import com.yt.seckill.entity.TOrderRecord;
 import com.yt.seckill.service.impl.TOrderRecordServiceImpl;
 import io.swagger.v3.oas.annotations.Operation;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * <p>
@@ -25,9 +28,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class TOrderRecordController {
     @Autowired
     TOrderRecordServiceImpl tOrderRecordService;
+
+    //每秒放行10个请求
+    RateLimiter rateLimiter = RateLimiter.create(100);
+
     @PostMapping("/create")
     @Operation(summary = "用户下单")
     public TOrderRecord createWrongOrder(String dataid) throws Exception {
+        if (!rateLimiter.tryAcquire(1000, TimeUnit.MILLISECONDS))
+            throw new RuntimeException("抢购失败，限流");
         TOrderRecord tOrderRecord = tOrderRecordService.createOrder(dataid);
         return tOrderRecord;
     }
